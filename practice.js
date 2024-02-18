21903f3bb216c5c01f27d4f9eca10ff63acbf95f
1asimdhali <s18237018448@nu.ac.bd>
2024-02-18 09:21:26 +0600

    plus minus number

---
+++ practice.js
@@ -0,0 +1,26 @@
+const plus = document.querySelector(".plus");
+const minus = document.querySelector(".minus");
+const num = document.querySelector(".num");
+
+let a =1;
+// plus.addEventListener("click", ()=>{
+//     a++;
+//     a=(a<10) ? "0" + a:a;
+//     num.innerText =a;
+//     });
+
+plus.addEventListener("click", ()=> {
+    if(a<10){
+        a++;
+        a=(a<10) ? "0" +a: a;
+        num.innerText =a;
+    }
+})
+
+minus.addEventListener("click",()=>{
+    if(a>1){
+        a--;
+        a=(a<10)?"0"+a:a;
+        num.innerText=a;
+    }
+})
